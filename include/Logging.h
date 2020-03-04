ce40d7618933de4ec53b5e18d8be79e388f06a77
cnweaver <cnweaver@gmail.com>
2020-03-04 14:46:51 -0600

    Rely on journald for checksums, to reduce log size

--- include/Logging.h
+++ include/Logging.h
@@ -9,15 +9,15 @@
 
 ///Log an informational message to stdout
 #define log_info(msg) \
-do{ std::cout << "INFO: [" << timestamp() << "] " << msg << std::endl; }while(0)
+do{ std::cout << "INFO: " << msg << std::endl; }while(0)
 
 ///Log that an error or problem has occurred to stderr
 #define log_warn(msg) \
-do{ std::cerr << "WARNING: [" << timestamp() << "] " << msg << std::endl; }while(0)
+do{ std::cerr << "WARNING: " << msg << std::endl; }while(0)
 
 ///Log that an error or problem has occurred to stderr
 #define log_error(msg) \
-do{ std::cerr << "ERROR: [" << timestamp() << "] " << msg << std::endl; }while(0)
+do{ std::cerr << "ERROR: " << msg << std::endl; }while(0)
 
 ///Log an error to stderr and abort the current activity by throwing an exception
 ///\throws std::runtime_error
@@ -25,7 +25,7 @@ do{ std::cerr << "ERROR: [" << timestamp() << "] " << msg << std::endl; }while(0
 do{ \
 	std::ostringstream str; \
 	str << msg; \
-	std::cerr << "FATAL: [" << timestamp() << "] " << str.str() << std::endl; \
+	std::cerr << "FATAL: " << str.str() << std::endl; \
 	throw std::runtime_error(str.str()); \
 }while(0)
 
