784e063c6b41c99d6d96095572f5ff7cf5e438c4
Lincoln Bryant <lincolnb@uchicago.edu>
2021-03-29 13:50:06 -0500

    Update sync_users.sh

--- resources/provisioner/sync_users.sh
+++ resources/provisioner/sync_users.sh
@@ -498,7 +498,7 @@ set_forward_file(){
 	USER="$1"
 	USER_HOME_DIR="$2"
 	USER_EMAIL="$3"
-	# Don't overwrite if the user already has a project file
+	# Don't overwrite if the user already has a .forward file
 	if [ -e "$USER_HOME_DIR/.forward" ]; then
 		return
 	fi
