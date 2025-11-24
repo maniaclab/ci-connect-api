ab76f97a2a11cb2c425e437898d1f2dff1b0a0bc
Judith Stephen <jlstephen@uchicago.edu>
2025-11-24 16:09:03 -0600

    only update ssh keys on one machine for nfs homes

--- resources/provisioner/sync_users.sh
+++ resources/provisioner/sync_users.sh
@@ -1202,7 +1202,9 @@ for USER in $USERS_TO_UPDATE; do
 	echo "Updating user $USER with groups $USER_GROUPS"
 	if [ ! "$DRY_RUN" ]; then
 		usermod -c "$USER_GECOS" -G "$USER_GROUPS" "$USER"
-		set_ssh_authorized_keys "$USER" "${HOME_DIR_ROOT}/${USER}" "$(/usr/bin/env echo "$USER_DATA" | jq -r '.public_key')"
+		if [ "$(hostname -f)" == "amundsen.grid.uchicago.edu" -o "$(hostname -f)" == "bastion.grid.uchicago.edu" -o "$(hostname -f)" == "ap23.uc.osg-htc.org" -o "$(hostname -f)" == "nfs.af.uchicago.edu" ]; then
+			set_ssh_authorized_keys "$USER" "${HOME_DIR_ROOT}/${USER}" "$(/usr/bin/env echo "$USER_DATA" | jq -r '.public_key')"
+		fi
 		# OSG specific: Try to pick out the first group to which the user belongs and set it as the default 'project'
 		# However, we must not pick 'osg', or any of the login node groups, so we remove these from the list.
 		FILTERED_USER_GROUPS=$(/usr/bin/env echo "$RAW_USER_GROUPS" | sed -e '/^'"$BASE_GROUP_NAME"'$/d' -e '/^'"$BASE_GROUP_NAME"'.login-nodes/d')
